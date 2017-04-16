2f567b675fda7c1ca9726bfe33cd419ce953ab04
merlinxcy <xcy_merlin@outlook.com>
2017-04-16 09:11:22 +0800

    同步服务端

---
+++ modbus/SynModbusServer.py
@@ -0,0 +1,27 @@
+from pymodbus.server.sync import StartTcpServer
+from pymodbus.server.sync import StartUdpServer
+from pymodbus.server.sync import StartSerialServer
+
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+from pymodbus.transaction import ModbusRtuFramer
+import logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+store = ModbusSlaveContext(
+    di = ModbusSequentialDataBlock(0, [17]*100),
+    co = ModbusSequentialDataBlock(0, [17]*100),
+    hr = ModbusSequentialDataBlock(0, [17]*100),
+    ir = ModbusSequentialDataBlock(0, [17]*100))
+context = ModbusServerContext(slaves=store, single=True)
+identity = ModbusDeviceIdentification()
+identity.VendorName  = 'Pymodbus'
+identity.ProductCode = 'PM'
+identity.VendorUrl   = 'http://github.com/bashwork/pymodbus/'
+identity.ProductName = 'Pymodbus Server'
+identity.ModelName   = 'Pymodbus Server'
+identity.MajorMinorRevision = '1.0'
+StartTcpServer(context, identity=identity, address=("192.168.237.129", 502))
