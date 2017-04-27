77d70ff2d672c2c7bee692b6a4a402ab1212a8ce
merlinxcy <xcy_merlin@outlook.com>
2017-04-27 18:04:57 +0800

    rm

--- socket/socket1.py
+++
@@ -1,9 +0,0 @@
-import socket
-host_name="192.168.237.111"
-host_port=1234
-#create socket
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect((host_name,host_port))
-client.send("Cisco 514 UDP Flood Denial of Service Vulnerability")
-response=client.recv(4096)
-print	response
