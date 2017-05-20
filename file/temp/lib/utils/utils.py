c16f58182fe67dc70cb7ca7e65558e3747b3f13c
merlinxcy <xcy_merlin@outlook.com>
2017-05-20 13:11:20 +0800

    日志备份

---
+++ file/temp/lib/utils/utils.py
@@ -0,0 +1,12 @@
+import re
+
+def find_in_list(target,str):
+	ans=list()
+	for i in target:
+		if i.find(str)!=-1:
+			#print "string has been find in target"
+			ans.append(i)
+	return ans
+
+def find_in_str(target,str):
+	pass
